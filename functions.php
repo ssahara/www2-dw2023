7eb8bda09e3eb35002edc8f153bb578ac3af5bce
Satoshi Sahara <sahara.satoshi@gmail.com>
2023-10-10 13:16:33 +0900

    エクストラフィードを抑制する

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ add_action( 'wp_enqueue_scripts', function () {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css' );
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() .'/style.css', array('parent-style'));
 });
+
+// エクストラフィードの生成を抑止する
+remove_action('wp_head', 'feed_links_extra', 3);
+// WPサイトフィード、全体のコメントフィードを生成する
+//add_theme_support( 'automatic-feed-links' );
+
