4200c61124fec3f0ae282fff3455b6a38ea979ef
Satoshi Sahara <sahara.satoshi@gmail.com>
2023-10-13 01:12:12 +0900

    不要コードを削除

--- index.php
+++ index.php
@@ -14,23 +14,11 @@ get_header();
         </p>
     </div>
     <p class="index-txt">※海外情報ニュースフラッシュは、経済産業省資源エネルギー庁の委託事業として、公益財団法人 原子力環境整備促進・資金管理センターが提供しています。</p>
-
-<h2 class="index-mh">最新記事</h2>
 <?php endif; ?>
-
-
-
-
-<?php if ( !is_home() && !is_front_page() ): ?>
-<?php if (function_exists('wp_page_numbers')) { wp_page_numbers(); } ?>
-<?php endif; ?>
-
-
-
-
 
 
 <?php
+    // 最新記事 を 以下の条件で検索
     $args = array(
         'post_type' => 'post',
         'category_name' => 'news,followup',
@@ -38,46 +26,26 @@ get_header();
     );
     $the_query = new WP_Query( $args );
 ?>
-
+    <h2 class="index-mh">最新記事</h2>
 <?php if ( $the_query->have_posts() ): ?>
     <div class="news-list" id="<?php the_ID() ?>">
         <ul>
-<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
             <li class="">
                 <div class="sya_postcontent">
                     <span class="sya_date"><?php the_time('Y-m-d'); ?> <span class="sya_sep"> </span></span>
                     <a href="<?php the_permalink() ?>" class="sya_postlink" rel="bookmark"><?php the_title() ?></a>
-<?php if (has_tag()==true): ?>
+        <?php if ( has_tag() ): ?>
                     <span class="sya_tags"><span class="sya_bracket">(</span>
                     <?php echo the_country_tags(); ?>
                     <span class="sya_bracket">)</span></span>
-<?php endif; ?>
+        <?php endif; ?>
                 </div>
             </li>
-<?php endwhile; ?>
+    <?php endwhile; ?>
         </ul>
     </div>
-<?php endif; ?>
-
-
-
-<?php if ( is_home() || is_front_page() ) : ?>
-
-    <p class="index-btn"><a href="<?php echo esc_url( home_url( '/') ); ?>?page_id=3111">全記事一覧へ</a></p>
-
-<?php endif; ?>
-
-
-<?php if ( !is_home() && !is_front_page() ) : ?>
-<?php if (function_exists('wp_page_numbers')): ?>
-    <?php wp_page_numbers(); ?>
-<?php else: ?>
-    <div class="navigation">
-        <div class="alignleft"><?php previous_posts_link(__('Newer entries', 'encyclopedia')) ?></div>
-        <div class="alignright"><?php next_posts_link(__('Older entries', 'encyclopedia')) ?></div>
-    </div>
-    <br />
-<?php endif; ?>
+    <p class="index-btn"><a href="<?php echo get_page_link(3111); ?>">全記事一覧へ</a></p>
 <?php endif; ?>
 
 
@@ -87,4 +55,4 @@ get_header();
     </div>
 
 
-<?php get_footer() ?>
+<?php get_footer(); ?>
