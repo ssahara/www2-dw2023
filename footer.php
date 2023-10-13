0649c1b572d04242003ff3e1afbe8877ce560c75
Satoshi Sahara <sahara.satoshi@gmail.com>
2023-10-13 14:33:29 +0900

    当サイトについてのリンク先

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 </div><!--home-->
 
 <div id="footer">
-<p><a href="https://www2.rwmc.or.jp/">諸外国での高レベル放射性廃棄物処分</a> 2002-2023 | <a href="/nf/?page_id=31954" title="about">当サイトについて</a></p>
+<p><a href="https://www2.rwmc.or.jp/">諸外国での高レベル放射性廃棄物処分</a> 2002-2023 | <a href="/about" title="about">当サイトについて</a></p>
 
 <p>このウェブサイトは、<a href="https://www.enecho.meti.go.jp" target="new">経済産業省資源エネルギー庁</a>の委託事業として、<a href="https://www.rwmc.or.jp">公益財団法人原子力環境整備促進・資金管理センター</a>が整備しています。</p>
 
