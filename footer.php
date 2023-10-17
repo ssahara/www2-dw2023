cdccc0028615b8ca793936809b17d0a645454a29
Satoshi Sahara <sahara.satoshi@gmail.com>
2023-10-17 18:39:48 +0900

    css define dokuwiki__footer

--- footer.php
+++ footer.php
@@ -6,11 +6,14 @@
 </div><!--wp__content-->
 </main><!--dokuwiki__content-->
 
-<footer id="footer">
+<!-- ********** FOOTER ********** -->
+<footer id="dokuwiki__footer">
+<div id="footer">
 <p><a href="https://www2.rwmc.or.jp/">諸外国での高レベル放射性廃棄物処分</a> 2002-2023 | <a href="/about" title="about">当サイトについて</a></p>
 
-<p>このウェブサイトは、<a href="https://www.enecho.meti.go.jp" target="new">経済産業省資源エネルギー庁</a>の委託事業として、<a href="https://www.rwmc.or.jp">公益財団法人原子力環境整備促進・資金管理センター</a>が整備しています。</p>
+<p>このウェブサイトは、<a href="https://www.enecho.meti.go.jp" target="_blank">経済産業省資源エネルギー庁</a>の委託事業として、<a href="https://www.rwmc.or.jp">公益財団法人原子力環境整備促進・資金管理センター</a>が整備しています。</p>
 
+</div><!-- footer -->
 </footer>
 
 </div><!--dokuwiki__site-->
